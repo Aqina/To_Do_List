b6d26e6e5b7cda5d6d2081fe0a13406cf08b9986
Debanjan <68193122+debanjangg@users.noreply.github.com>
2024-12-09 18:18:36 +0530

    Changed homepage from https://<domain:port>/FunctionApp/index to https://<domain:port>

--- myapp/urls.py
+++ myapp/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('FunctionApp/',include('FunctionApp.urls'))
+    path('',include('FunctionApp.urls'))
 ]
  
